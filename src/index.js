6f1949c4a60e95144492f318f91947f93c94f853
jj-jamen <hello.jjjamen@gmail.com>
2024-07-31 17:43:39 +0800

    building in progress

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <NavMenu />
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Main component
+function App() {
+  return (
+    <main>
+      <h1 className="gradientText">Building in progress...</h1>
+    </main>
+  );
+}
+
+function NavMenu() {
+  return "this is the navbar";
+}
