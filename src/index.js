762713a4db26b6031eee80341b088a22db73d80e
jj-jamen <hello.jjjamen@gmail.com>
2024-08-01 13:53:34 +0800

    added new features

--- src/index.js
+++ src/index.js
@@ -1,24 +1,97 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import quoteData from "./quoteData.json";
+import IconButton from "@mui/material/IconButton";
+import CachedIcon from "@mui/icons-material/Cached";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <WebTitle />
+    <Quotes />
     <NavMenu />
-    <App />
+    <Socials />
   </React.StrictMode>
 );
 
-// Main component
-function App() {
+function WebTitle() {
   return (
-    <main>
-      <h1 className="gradientText">Building in progress...</h1>
-    </main>
+    <div className="text-is-centered title">
+      <h1 className="mont-300-normal text-is-centered gradientText">
+        John Jeffrey Jamen
+      </h1>
+    </div>
+  );
+}
+
+function Quotes() {
+  // Function to get a random quote
+  function getQuote() {
+    const randomIndex = Math.floor(Math.random() * quoteData.length);
+    return quoteData[randomIndex];
+  }
+
+  // State to hold the current quote
+  const [quote, setQuote] = useState(getQuote());
+
+  // Function to handle button click and get a new quote
+  function newQuote() {
+    setQuote(getQuote());
+  }
+
+  // Render the UI
+  return (
+    <div className="quote-container">
+      <div className="quotes">
+        <p className="quoteText">{quote.quote}</p>
+      </div>
+      <div className="quotes">
+        <p>
+          <em>- {quote.author} -</em>
+        </p>
+      </div>
+      <div className="quotes">
+        <IconButton onClick={newQuote}>
+          <CachedIcon className="qoute-button" />
+        </IconButton>
+      </div>
+    </div>
   );
 }
 
 function NavMenu() {
-  return "this is the navbar";
+  return (
+    <div className="navContainer">
+      <div className="navItem">Jobs</div>
+      <div className="navItem">Skills</div>
+      <div className="navItem">Prototypes</div>
+      <div className="navItem">Education</div>
+    </div>
+  );
+}
+
+function Socials() {
+  return (
+    <div className="socialIconsContainer">
+      <div className="socialIcons">
+        <IconButton
+          href="https://github.com/jj-jamen"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <GitHubIcon />
+        </IconButton>
+        <IconButton
+          href="https://www.linkedin.com/in/jjjamen/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <LinkedInIcon />
+        </IconButton>
+      </div>
+    </div>
+  );
 }
